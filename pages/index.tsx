15b3061683d452d62229f8b56bd37ecf02a4e0de
Fabricio Ferreira <fabricio@fabriciofs.eti.br>
2020-09-22 05:52:55 -0300

    create and config the project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from './Home.module.css'
 
 export default function Home() {
   return (
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.ts!</a>
         </h1>
 
         <p className={styles.description}>
